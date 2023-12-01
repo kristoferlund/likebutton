f5163876383f43350dd2b8929069c26525d749ab
Kristofer <kristofer@fmckl.se>
2023-12-01 19:10:02 +0100

    allow only optimism

--- src/wagmi/wagmiConfig.ts
+++ src/wagmi/wagmiConfig.ts
@@ -2,15 +2,15 @@
 
 import "@rainbow-me/rainbowkit/styles.css";
 
-import { arbitrum, base, mainnet, optimism, polygon, zora } from "wagmi/chains";
 import { configureChains, createConfig } from "wagmi";
 
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { optimism } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [optimism],
   [
     alchemyProvider({ apiKey: "9gxw46BCBj2lWs5hT3Z0mvgm-333uhVy" }),
     publicProvider(),
